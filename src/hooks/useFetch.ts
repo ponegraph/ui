cf54dc4bb3c995b6d702e0d70621ae4819fece1e
nseptio <septio.nugroho@ui.ac.id>
2024-11-18 22:38:07 +0700

    dev: add custom hook for data fetching

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+type FetchState<T> = {
+  data: T | null;
+  loading: boolean;
+  error: string | null;
+};
+
+// Caveat: This custom hook is only for GET method and doesn't handle headers, body, etc.
+export const useFetch = <T>(url: string): FetchState<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err.message || "Something went wrong");
+        } else {
+          setError("An unknown error occurred");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
